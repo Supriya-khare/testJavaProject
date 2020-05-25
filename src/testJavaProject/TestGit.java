6985df5136720d313c53dfd4d5d898ac5b9a181e
supri_200 <supri_200@Supriya-PC>
2020-05-25 16:04:17 -0400

    I am dev 2

--- src/testJavaProject/TestGit.java
+++ src/testJavaProject/TestGit.java
@@ -1,11 +1,12 @@
 package testJavaProject;
 
 public class TestGit {
-	public static  int[] data = {1, 5, 7, 8,9};
+	public static  Integer [] data = {1, 5, 7, 8,9, null};
 	
 	public static void main(String[] args) {
 		for (int i = 0; i < data.length; i++) {
 			System.out.println(  i + "- " + data[i]);
+			System.out.println("size is " + data.length);
 		}
 	}
 }
