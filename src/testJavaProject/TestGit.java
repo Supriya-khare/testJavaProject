426d7ffa1cb7933ce660f94eae2eaea246ad17ee
supri_000 <supri_000@Supriya-PC>
2020-05-25 15:04:10 -0400

    commit local 1

--- src/testJavaProject/TestGit.java
+++ src/testJavaProject/TestGit.java
@@ -1,10 +1,10 @@
 package testJavaProject;
 
 public class TestGit {
-	public static  int[] data = {1, 5, 7, 8,9};
+	public static  int[] data = {1, 2,  5, 7, 8,9, 10};
 	
 	public static void main(String[] args) {
-		for (int i = 0; i < data.length; i++) {
+		for (int i: data ) {
 			System.out.println(  i + "- " + data[i]);
 		}
 	}
