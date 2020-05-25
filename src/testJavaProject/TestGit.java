3dc49d9046e70486c4ff2188e9296b7ebca56246
supri_000 <supri_000@Supriya-PC>
2020-05-25 14:55:28 -0400

    simple testJava class

---
+++ src/testJavaProject/TestGit.java
@@ -0,0 +1,11 @@
+package testJavaProject;
+
+public class TestGit {
+	public static  int[] data = {1, 5, 7, 8,9};
+	
+	public static void main(String[] args) {
+		for (int i = 0; i < data.length; i++) {
+			System.out.println(  i + "- " + data[i]);
+		}
+	}
+}
