fc84a82864a541c3ffab57a5f16e701af133fe1a
supri_000 <supri_000@Supriya-PC>
2020-05-25 16:38:37 -0400

    small chanhges

--- src/testJavaProject/TestGit.java
+++ src/testJavaProject/TestGit.java
@@ -4,8 +4,11 @@ public class TestGit {
 	public static  int[] data = {1, 2,  5, 7, 8,9, 10};
 	
 	public static void main(String[] args) {
+		int sum = 0;
 		for (int i: data ) {
 			System.out.println(  i + "- " + data[i]);
-		}
+			sum += data[i];
+		} 
+		System.out.println(" sum is " + sum);
 	}
 }
