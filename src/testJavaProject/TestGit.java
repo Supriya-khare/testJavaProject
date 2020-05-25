4ecb388a117f78e4278957902724374fed4efce4
supri_000 <supri_000@Supriya-PC>
2020-05-25 16:13:22 -0400

    only local

--- src/testJavaProject/TestGit.java
+++ src/testJavaProject/TestGit.java
@@ -3,6 +3,9 @@ package testJavaProject;
 public class TestGit {
 	public static  Integer [] data = {1, 5, 7, 8,9, null};
 	
+	/**
+	 * main function for test
+	 */
 	public static void main(String[] args) {
 		for (int i = 0; i < data.length; i++) {
 			System.out.println(  i + "- " + data[i]);
